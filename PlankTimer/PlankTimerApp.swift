e09971e35c859254f6e77bd2918ba309eb668e8f
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-07-04 16:36:51 -0500

    added tab bar

--- PlankTimer/PlankTimerApp.swift
+++ PlankTimer/PlankTimerApp.swift
@@ -2,16 +2,36 @@
 //  PlankTimerApp.swift
 //  PlankTimer
 //
-//  Created by Jayce Sagvold on 7/3/23.
+//  Created by Jayce Sagvold on 7/4/23.
 //
 
 import SwiftUI
 
 @main
-struct PlankTimerApp: App {
+struct OneHabitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                ContentView()
+                    .badge(2)
+                    .tabItem {
+                        Label("Exercise", systemImage: "timer")
+                    }
+                HistoryView()
+                    .tabItem {
+                        Label("History", systemImage: "archivebox.fill")
+                    }
+                
+                GuidanceView()
+                    .tabItem {
+                        Label("Guidance", systemImage: "questionmark.folder.fill")
+                    }
+                
+                SettingsView()
+                    .tabItem {
+                        Label("Settings", systemImage: "gear")
+                    }
+            }
         }
     }
 }

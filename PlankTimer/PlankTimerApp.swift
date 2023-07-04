1a4ecdec614d4b0502cdf13991386cc412c26e91
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-07-03 20:20:15 -0500

    first commit

---
+++ PlankTimer/PlankTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlankTimerApp.swift
+//  PlankTimer
+//
+//  Created by Jayce Sagvold on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PlankTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

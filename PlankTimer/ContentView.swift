246f01e82311aaf240c387a851cc94eb83183e0a
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-07-04 08:10:53 -0500

    first commit

---
+++ PlankTimer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PlankTimer
+//
+//  Created by Jayce Sagvold on 7/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
